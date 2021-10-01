1480dce0d5a2cc94a067b87254bc9a2dedd89b0c
xzacharb <zachar.branislav09@gmail.com>
2021-10-01 21:16:27 +0200

    application properties updated

---
+++ authSvc/src/test/java/com/xzacharb/authsvc/AuthSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xzacharb.authsvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
